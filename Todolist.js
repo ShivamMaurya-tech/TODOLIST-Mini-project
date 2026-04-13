a6f166f3ee2dc9f810e1cc7391d5a1d085e0a6e1
Shivam Maurya <shivammaurya224466@gmail.com>
2026-04-13 21:24:51 +0530

    todolist mini project

---
+++ Todolist.js
@@ -0,0 +1,120 @@
+let mybtn=document.querySelector(".btn1");
+let inputfield=document.querySelector("#inputbox");
+
+//let createdbtn=document.querySelector(".btn2");
+
+let addelement=document.querySelector(".addingelement");
+let Deletebtn=document.querySelector(".delbtn");
+// let maintodoelement=document.querySelector(".divtodoelement");
+
+const handlebutton=()=>{
+    addtodolist();
+};
+
+
+
+
+const gettodolistfromlocal=()=>{
+    return JSON.parse(localStorage.getItem("Youtubeplaylist"));
+}
+
+const addtodolistlocalstorage=(elementarray=>{
+return localStorage.setItem("YoutubeList",JSON.stringify(elementarray));
+});
+
+
+
+  let  elementarray=gettodolistfromlocal()||[];
+   const addtododynamiclist=(element)=>{
+    // addtodolist(element);
+    
+    let divelement=document.createElement('div');
+    divelement.classList.add(".divtodoelement");
+    divelement.innerHTML=`<li>${element}</li>
+    <button class="delbtn">Delete</button>`;
+    
+addelement.append(divelement);
+
+   }
+
+
+
+
+const addtodolist=()=>{
+   
+
+    const todolistvalue=inputfield.value.trim();
+    if(!todolistvalue=="" && !elementarray.includes(todolistvalue)){
+    addtododynamiclist(todolistvalue);
+
+    // let divelement=document.createElement('div');
+    // divelement.classList.add(".divtodoelement");
+    // divelement.innerHTML=`<li>${inputfield.value}</li>
+    // <button class="delbtn">Delete</button>`;
+
+    elementarray.push(todolistvalue);
+    localStorage.setItem("Youtubeplaylist",JSON.stringify(elementarray));
+    elementarray=[...new Set(elementarray)];
+    console.log(elementarray);
+
+
+
+// JSON.parse(localStorage.getItem("Youtubeplaylist"));
+
+// addelement.append(divelement);
+inputfield.value="";
+
+    }
+
+};
+
+
+
+
+
+
+
+const showtodolist=()=>{
+
+    console.log(elementarray);
+   elementarray.forEach(element => {
+        addtododynamiclist(element);
+        
+    });
+}
+showtodolist();
+
+mybtn.addEventListener('click',handlebutton);
+
+
+
+
+//delete the data 
+addelement.addEventListener('click',(e)=>{
+    e.preventDefault();
+    if(e.target.classList.contains("delbtn")){
+
+    
+    removetodoelement(e);}
+});
+
+
+
+const removetodoelement=(e)=>{
+    const removetodoele=e.target;
+    let deletetodoelementcontent=removetodoele.previousElementSibling.innerText;
+   
+    let parentelement=removetodoele.parentElement;
+     console.log(deletetodoelementcontent);
+    parentelement.remove();
+    
+
+    elementarray=elementarray.filter((currenttodo)=>{
+
+return currenttodo!=deletetodoelementcontent.toLowerCase();
+    })
+console.log(elementarray);
+addtodolistlocalstorage(elementarray);
+
+}
+
